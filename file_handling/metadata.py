017528a4b433fc63e865f6ea5cc2154065269972
henriettestadler <henriette.stadler@icloud.com>
2026-07-06 16:31:28 +0200

    All files regarding file handling and loaded are now put into one folder

---
+++ file_handling/metadata.py
@@ -0,0 +1,195 @@
+# This Python file uses the following encoding: utf-8
+from PySide6.QtWidgets import QDialog, QVBoxLayout
+import SimpleITK as sitk
+from file_handling.mri_volume import MRIVolume
+
+class Metadata:
+    def __init__(self,MW):
+        self.MW = MW
+        self.ui = MW.ui
+        self.LoadMRI = MW.LoadMRI
+        self.ui.pushButton_metadata.clicked.connect(self.show_metadata)
+        self.ui.pushButton_changeSpacing.clicked.connect(self.change_spacing)
+        self.ui.pushButton_SaveSpacing.clicked.connect(self.save_new_spacing)
+        self.ui.pushButton_SaveMetadata.clicked.connect(self.set_metadata)
+        self.ui.pushButton_reorient.clicked.connect(self.reorient_volume)
+
+        self.ui.doubleSpinBox_spax.valueChanged.connect(lambda val: self.changed_parameters_spacing(val,'x'))
+        self.ui.doubleSpinBox_spay.valueChanged.connect(lambda val: self.changed_parameters_spacing(val,'y'))
+        self.ui.doubleSpinBox_spaz.valueChanged.connect(lambda val: self.changed_parameters_spacing(val,'z'))
+        self.ui.doubleSpinBox_fovx.valueChanged.connect(lambda val: self.changed_parameters_fov(val,'x'))
+        self.ui.doubleSpinBox_fovy.valueChanged.connect(lambda val: self.changed_parameters_fov(val,'y'))
+        self.ui.doubleSpinBox_fovz.valueChanged.connect(lambda val: self.changed_parameters_fov(val,'z'))
+
+
+    def show_metadata(self):
+        if hasattr(self, "popup") and self.popup.isVisible():
+            self.popup.raise_()
+            self.popup.activateWindow()
+            return
+        self.popup = PopupDialog(parent=self.MW,ui_widget=self.ui.frame_metadata)
+        self.popup.resize(300, 300)
+        self.popup.show()
+        self.ui.pushButton_cancel_metadata.clicked.connect(self.popup.close)
+        self.fill_metadata()
+
+    def change_spacing(self):
+        if hasattr(self, "spacing_popup") and self.spacing_popup.isVisible():
+            self.popup.raise_()
+            self.popup.activateWindow()
+            return
+        self.spacing_popup = PopupDialog(parent=self.MW,ui_widget=self.ui.frame_spacing)
+        self.spacing_popup.resize(300, 300)
+        self.spacing_popup.show()
+        self.ui.pushButton_cancel_spacing.clicked.connect(self.spacing_popup.close)
+        self.fill_spacing_metadata()
+
+    def fill_metadata(self):
+        self.volume = self.LoadMRI.volumes[0]
+        image = self.LoadMRI.volumes[0].oriented_ref_image
+        #dimensions
+        self.ui.spinBox_dimx.setValue(self.volume.slices[0].shape[2])
+        self.ui.spinBox_dimy.setValue(self.volume.slices[0].shape[1])
+        self.ui.spinBox_dimz.setValue(self.volume.slices[0].shape[0])
+        #spacing
+        self.ui.doubleSpinBox_spacingx.setValue(self.volume.spacing[2])
+        self.ui.doubleSpinBox_spacingy.setValue(self.volume.spacing[1])
+        self.ui.doubleSpinBox_spacingz.setValue(self.volume.spacing[0])
+        #origin
+        self.ui.doubleSpinBox_originx.setValue(image.GetOrigin()[2])
+        self.ui.doubleSpinBox_originy.setValue(image.GetOrigin()[1])
+        self.ui.doubleSpinBox_originz.setValue(image.GetOrigin()[0])
+        #orientation
+        orient_filter = sitk.DICOMOrientImageFilter()
+        current_orient = orient_filter.GetOrientationFromDirectionCosines(image.GetDirection())
+        self.ui.lineEdit_DicomOrient.setText(current_orient)
+
+        #affine
+        direction = image.GetDirection()
+        self.ui.lineEdit_direction.setText(f"{direction}")
+
+        #Intensity Range
+        self.ui.doubleSpinBox_maxIntensity.setValue(self.volume.slices[0].max())
+        self.ui.doubleSpinBox_minIntensity.setValue(self.volume.slices[0].min())
+
+
+
+    def save_new_spacing(self):
+        self.ui.doubleSpinBox_spacingx.setValue(self.ui.doubleSpinBox_spax.value())
+        self.ui.doubleSpinBox_spacingy.setValue(self.ui.doubleSpinBox_spay.value())
+        self.ui.doubleSpinBox_spacingz.setValue(self.ui.doubleSpinBox_spaz.value())
+
+        self.spacing_popup.close()
+
+
+    def fill_spacing_metadata(self):
+        self.ui.doubleSpinBox_spax.blockSignals(True)
+        self.ui.doubleSpinBox_spay.blockSignals(True)
+        self.ui.doubleSpinBox_spaz.blockSignals(True)
+        self.ui.doubleSpinBox_fovx.blockSignals(True)
+        self.ui.doubleSpinBox_fovy.blockSignals(True)
+        self.ui.doubleSpinBox_fovz.blockSignals(True)
+        #dimensions
+        self.ui.spinBox_dimex.setValue(self.volume.slices[0].shape[2])
+        self.ui.spinBox_dimey.setValue(self.volume.slices[0].shape[1])
+        self.ui.spinBox_dimez.setValue(self.volume.slices[0].shape[0])
+        #spacing
+        self.ui.doubleSpinBox_spax.setValue(self.volume.spacing[2])
+        self.ui.doubleSpinBox_spay.setValue(self.volume.spacing[1])
+        self.ui.doubleSpinBox_spaz.setValue(self.volume.spacing[0])
+        #field of view
+        fov_x = self.volume.slices[0].shape[2] * self.volume.spacing[2]
+        fov_y = self.volume.slices[0].shape[1] * self.volume.spacing[1]
+        fov_z = self.volume.slices[0].shape[0] * self.volume.spacing[0]
+        self.ui.doubleSpinBox_fovx.setValue(fov_x)
+        self.ui.doubleSpinBox_fovy.setValue(fov_y)
+        self.ui.doubleSpinBox_fovz.setValue(fov_z)
+        self.ui.doubleSpinBox_spax.blockSignals(False)
+        self.ui.doubleSpinBox_spay.blockSignals(False)
+        self.ui.doubleSpinBox_spaz.blockSignals(False)
+        self.ui.doubleSpinBox_fovx.blockSignals(False)
+        self.ui.doubleSpinBox_fovy.blockSignals(False)
+        self.ui.doubleSpinBox_fovz.blockSignals(False)
+
+    def changed_parameters_fov(self,val,axis):
+        self.ui.doubleSpinBox_spax.blockSignals(True)
+        self.ui.doubleSpinBox_spay.blockSignals(True)
+        self.ui.doubleSpinBox_spaz.blockSignals(True)
+
+        if axis=='x':
+            spa_x = val / self.volume.slices[0].shape[2]
+            self.ui.doubleSpinBox_spax.setValue(spa_x)
+        elif axis=='y':
+            spa_y = val / self.volume.slices[0].shape[1]
+            self.ui.doubleSpinBox_spay.setValue(spa_y)
+        elif axis=='z':
+            spa_z = val / self.volume.slices[0].shape[0]
+            self.ui.doubleSpinBox_spaz.setValue(spa_z)
+        self.ui.doubleSpinBox_spax.blockSignals(False)
+        self.ui.doubleSpinBox_spay.blockSignals(False)
+        self.ui.doubleSpinBox_spaz.blockSignals(False)
+
+    def changed_parameters_spacing(self,val,axis):
+        self.ui.doubleSpinBox_fovx.blockSignals(True)
+        self.ui.doubleSpinBox_fovy.blockSignals(True)
+        self.ui.doubleSpinBox_fovz.blockSignals(True)
+        if axis=='x':
+            fov_x = val * self.volume.slices[0].shape[2]
+            self.ui.doubleSpinBox_fovx.setValue(fov_x)
+        elif axis=='y':
+            fov_y = val * self.volume.slices[0].shape[1]
+            self.ui.doubleSpinBox_fovy.setValue(fov_y)
+        elif axis=='z':
+            fov_z = val * self.volume.slices[0].shape[0]
+            self.ui.doubleSpinBox_fovz.setValue(fov_z)
+
+        self.ui.doubleSpinBox_fovx.blockSignals(False)
+        self.ui.doubleSpinBox_fovy.blockSignals(False)
+        self.ui.doubleSpinBox_fovz.blockSignals(False)
+
+    def set_metadata(self):
+        new_spacing = [self.ui.doubleSpinBox_spacingz.value(),self.ui.doubleSpinBox_spacingy.value(),self.ui.doubleSpinBox_spacingx.value()]
+
+        img = sitk.ReadImage(self.LoadMRI.volumes[0].file_path)
+        img.SetSpacing(new_spacing)
+        sitk.WriteImage(img, self.LoadMRI.volumes[0].file_path)
+
+        self.MW.restart_gui(self.LoadMRI.volumes[0].file_path,True,False)
+
+
+    def reorient_volume(self):
+        data_index = 0
+        if self.ui.pushButton_reorient.isChecked():
+            DICOMOrient = 'LAS'
+            file_name = self.LoadMRI.volumes[data_index].file_path
+            self.LoadMRI.volumes[data_index] = MRIVolume.from_file(file_name,DICOMOrient)
+            self.ui.pushButton_reorient.setText('Reorient to RAS')
+        else:
+            DICOMOrient = 'RAS'
+            file_name = self.LoadMRI.volumes[data_index].file_path
+            self.LoadMRI.volumes[data_index] = MRIVolume.from_file(file_name,DICOMOrient)
+            self.ui.pushButton_reorient.setText('Reorient to LAS')
+        #orientation
+        orient_filter = sitk.DICOMOrientImageFilter()
+        current_orient = orient_filter.GetOrientationFromDirectionCosines(self.LoadMRI.volumes[data_index].oriented_ref_image.GetDirection())
+        self.ui.lineEdit_DicomOrient.setText(current_orient)
+
+        #3D
+        for vn in 'axial','coronal','sagittal':
+            self.LoadMRI.update_slices(0,0,vn)
+
+
+class PopupDialog(QDialog):
+    """
+        Class for pop/up dialog for Metadata.
+    """
+    def __init__(self, parent=None, ui_widget=None):
+        super().__init__(parent)
+        self.setWindowTitle("Manual Control Adjustments")
+        layout = QVBoxLayout(self)
+        layout.addWidget(ui_widget)
+
+    def closeEvent(self, event):
+        # Instead of destroying, just hide the window
+        self.hide()
+        event.ignore()
---
+++ file_handling/resample_data.py
@@ -0,0 +1,512 @@
+# This Python file uses the following encoding: utf-8
+import SimpleITK as sitk
+import os
+from core.load_MRI_file import LoadMRI
+from file_handling.mri_volume import MRIVolume
+
+class ResampleData:
+    """
+    Handles resampling of 3D MRI image data to fixed voxel sizes (100 um or 25 um). Once resampled to 100um, the new resampled image is opened and displayed.
+    Based on the ResampleAction function from FSLeyes (open-source software).
+    """
+
+    def __init__(self,LoadMRI):
+        """ Initialize the ResampleData class. """
+        self.LoadMRI = LoadMRI
+
+
+
+    def resampling100um(self,index):
+        """
+            Resample the main MRI image to a z-spacing of 100um).
+            Applies padding at the end to avoid black slices after resampling.
+        """
+        file_name = self.LoadMRI.volumes[index].file_path
+        img = sitk.ReadImage(file_name)
+        img = sitk.DICOMOrient(img, self.LoadMRI.volumes[0].DICOMOrient)
+        old_size = img.GetSize()
+
+        old_spacing = self.LoadMRI.volumes[index].spacing[::-1] #x,y,z
+        new_spacing = [old_spacing[0],old_spacing[1],0.1] #x,y,z
+
+        new_size = [
+            int(round(osz * ospc / nspc))
+            for osz, ospc, nspc in zip(old_size, old_spacing, new_spacing)
+        ]
+
+        new_newspacing = [
+            old_sz * old_sp / new_sz
+            for old_sz, old_sp, new_sz in zip(old_size, old_spacing, new_size)
+        ]
+
+        # Get last slice intensities to avoid blackness in the last slides
+        last_slice_array = sitk.GetArrayFromImage(img)[-1,:,:]
+        edge_value = float(last_slice_array.mean())
+
+        last_slice = img[:, :, -1]
+
+        # Repeat the last slice N times to extend volume
+        origin = img.GetOrigin()
+        pad_slices = 20  # number of repeated slices
+        replicated = [last_slice] * pad_slices
+        replicated_stack = sitk.JoinSeries(replicated)
+
+        replicated_stack.SetSpacing((old_spacing[0], old_spacing[1], old_spacing[2]))
+        replicated_stack.SetOrigin((origin[0], origin[1], origin[2] + old_spacing[2]*old_size[2]))
+        replicated_stack.SetDirection(img.GetDirection())
+        image_padded = sitk.Paste(
+            sitk.ConstantPad(img, [0, 0, 0], [0, 0, pad_slices], 0),
+            replicated_stack,
+            replicated_stack.GetSize(),
+            destinationIndex=[0, 0, img.GetSize()[2]]
+        )
+        resampled = sitk.Resample(
+            image_padded,
+            new_size,
+            sitk.Transform(),
+            sitk.sitkLinear,
+            image_padded.GetOrigin(),
+            new_newspacing,
+            image_padded.GetDirection(),
+            edge_value,
+            image_padded.GetPixelID()
+        )
+        filename_end = 'resampled100um.nii.gz'
+        file_name = self.save_as_niigz(resampled,filename_end,index)
+        self.file_name100um = file_name
+
+        return file_name
+
+
+    def resampling25um(self,index):
+        """
+            Resample the main MRI image sequentially in z, y, and x directions
+            to achieve a final voxel size of 25 µm isotropic.
+        """
+        file_name = self.LoadMRI.volumes[index].file_path
+        img = sitk.ReadImage(file_name)
+        img = sitk.DICOMOrient(img, self.LoadMRI.volumes[0].DICOMOrient)
+        old_spacing = self.LoadMRI.volumes[index].spacing[::-1] #x,y,z
+
+        old_size = img.GetSize()
+        # first resample in z
+        new_spacing = [old_spacing[0],old_spacing[1],0.025] #x,y,z
+
+        new_size = [
+            int(round(osz * ospc / nspc))
+            for osz, ospc, nspc in zip(old_size, old_spacing, new_spacing)
+        ]
+
+        new_newspacing = [
+            old_sz * old_sp / new_sz
+            for old_sz, old_sp, new_sz in zip(old_size, old_spacing, new_size)
+        ]
+
+        # Get last slice intensities to avoid blackness in the last slides
+        last_slice_array = sitk.GetArrayFromImage(img)[-1,:,:]
+        edge_value = float(last_slice_array.mean())
+
+
+        last_slice = img[:, :, -1]
+
+        # Repeat the last slice N times to extend volume
+        origin = img.GetOrigin()
+        pad_slices = 20 # number of repeated slices
+        replicated = [last_slice] * pad_slices
+        replicated_stack = sitk.JoinSeries(replicated)
+        replicated_stack.SetSpacing((old_spacing[0], old_spacing[1], old_spacing[2]))
+        replicated_stack.SetOrigin((origin[0], origin[1], origin[2] + old_spacing[2]*old_size[2]))
+        replicated_stack.SetDirection(img.GetDirection())
+
+        image_padded = sitk.Paste(
+            sitk.ConstantPad(img, [0, 0, 0], [0, 0, pad_slices], 0),
+            replicated_stack,
+            replicated_stack.GetSize(),
+            destinationIndex=[0, 0, img.GetSize()[2]]
+        )
+
+        resampled_z = sitk.Resample(
+            image_padded,
+            new_size,
+            sitk.Transform(),
+            sitk.sitkLinear,
+            image_padded.GetOrigin(),
+            new_newspacing,
+            image_padded.GetDirection(),
+            edge_value,
+            image_padded.GetPixelID()
+        )
+        # resample in y
+        old_spacing = resampled_z.GetSpacing()
+        old_size = resampled_z.GetSize()
+        new_spacing = [old_spacing[0],0.025,old_spacing[2]] #x,y,z
+
+        new_size = [
+            int(round(osz * ospc / nspc))
+            for osz, ospc, nspc in zip(old_size, old_spacing, new_spacing)
+        ]
+
+        new_newspacing = [
+            old_sz * old_sp / new_sz
+            for old_sz, old_sp, new_sz in zip(old_size, old_spacing, new_size)
+        ]
+
+        # Get last slice intensities to avoid blackness in the last slides
+        last_slice_array = sitk.GetArrayFromImage(resampled_z)[:,-1,:]
+        edge_value = float(last_slice_array.mean())
+
+        img = resampled_z
+        last_slice = img[:, -1:, :]
+
+        ## Repeat the last slice N times to extend volume
+        origin = resampled_z.GetOrigin()
+        pad_slices = 20  # number of repeated slices
+        img_padded = sitk.ConstantPad(
+            img,
+            padLowerBound=[0, 0, 0],
+            padUpperBound=[0, pad_slices, 0],
+            constant=0  # temporary, will be overwritten
+        )
+
+        last_slice = sitk.Extract(
+            img_padded,
+            size=[img.GetSize()[0], 1, img.GetSize()[2]],
+            index=[0, img.GetSize()[1]-1, 0]
+        )
+
+        image_padded = img_padded
+        for i in range(pad_slices):
+            image_padded = sitk.Paste(
+                destinationImage=img_padded,
+                sourceImage=last_slice,
+                sourceSize=last_slice.GetSize(),
+                destinationIndex=[0, img.GetSize()[1]+i, 0]
+            )
+
+        resampled_zy = sitk.Resample(
+            image_padded,
+            new_size,
+            sitk.Transform(),
+            sitk.sitkLinear,
+            image_padded.GetOrigin(),
+            new_newspacing,
+            image_padded.GetDirection(),
+            edge_value,
+            image_padded.GetPixelID()
+        )
+
+        # resample in x
+        old_spacing = resampled_zy.GetSpacing()
+        old_size = resampled_zy.GetSize()
+        new_spacing = [0.025, old_spacing[1],old_spacing[2]]
+
+        new_size = [
+            int(round(osz * ospc / nspc))
+            for osz, ospc, nspc in zip(old_size, old_spacing, new_spacing)
+        ]
+
+        new_newspacing = [
+            old_sz * old_sp / new_sz
+            for old_sz, old_sp, new_sz in zip(old_size, old_spacing, new_size)
+        ]
+
+        # Get last slice intensities to avoid blackness in the last slides
+        last_slice_array = sitk.GetArrayFromImage(resampled_zy)[-1,:,:]
+        edge_value = float(last_slice_array.mean())
+
+        img = resampled_zy
+        last_slice = img[-1:, :, :]
+
+        ## Repeat the last slice N times to extend volume
+        origin = resampled_zy.GetOrigin()
+        pad_slices = 20  # number of repeated slices
+        img_padded = sitk.ConstantPad(
+            img,
+            padLowerBound=[0, 0, 0],
+            padUpperBound=[pad_slices, 0, 0],
+            constant=0  # temporary, will be overwritten
+        )
+
+        last_slice = sitk.Extract(
+            img_padded,
+            size=[1, img.GetSize()[1], img.GetSize()[2]],
+            index=[img.GetSize()[0]-1, 0, 0]
+        )
+
+        image_padded_x = img_padded
+        for i in range(pad_slices):
+            image_padded_x = sitk.Paste(
+                destinationImage=image_padded_x,
+                sourceImage=last_slice,
+                sourceSize=last_slice.GetSize(),
+                destinationIndex=[img.GetSize()[0]+i,0, 0]
+            )
+
+        resampled = sitk.Resample(
+            image_padded_x,
+            new_size,
+            sitk.Transform(),
+            sitk.sitkLinear,
+            image_padded_x.GetOrigin(),
+            new_newspacing,
+            image_padded_x.GetDirection(),
+            edge_value,
+            image_padded_x.GetPixelID()
+        )
+        filename_end = 'resampled.nii.gz'
+        save_path = self.save_as_niigz(resampled,filename_end,index)
+
+
+        return save_path
+
+
+
+    def save_as_niigz(self,image,filename_end:str,index:int):
+        """
+        Save the current label volume as a NIfTI (.nii.gz) file.
+        """
+
+        file_name = self.LoadMRI.volumes[index].file_path[:-7]
+        default_name = f"{file_name}_{filename_end}"
+        save_path = os.path.join(self.LoadMRI.session_path, default_name)
+        sitk.WriteImage(image, save_path)
+        return save_path
+
+
+    def open_as_new_file(self,buttons_gui3d,MW):
+        """
+            Replace the currently loaded MRI file with the new resampled one.
+            Clears old renderers, actors, and measurement lines from the GUI.
+        """
+        data_index = 0
+        #new volume and spacing
+        self.LoadMRI.volumes[data_index] = MRIVolume.from_file(self.file_name100um)
+        self.LoadMRI.is_first_slice = False
+
+
+        #delete measurement actors
+        if hasattr(MW,'Measurement'):
+            for view_name, line_actor,line_slice_index,text_actor,line,dashed_lines,points in MW.Measurement.measurement_lines:
+                renderer = MW.Measurement.measurement_renderer[view_name]
+                renderer.RemoveActor(line_actor)
+                renderer.RemoveActor(dashed_lines[1])
+                renderer.RemoveActor(dashed_lines[3])
+                renderer.RemoveActor(points[2])
+                text_actor.SetVisibility(0)
+            MW.Measurement.measurement_lines = []
+
+        for idx in self.LoadMRI.minimap.minimap_renderers:
+            for vn in self.LoadMRI.minimap.minimap_renderers[idx]:
+                self.LoadMRI.minimap.minimap_renderers[idx][vn].RemoveAllViewProps()
+            self.LoadMRI.minimap.minimap_renderers[idx] = {}
+
+
+        for idx in self.LoadMRI.renderers:
+            for vn in self.LoadMRI.renderers[idx]:
+                self.LoadMRI.renderers[idx][vn].RemoveAllViewProps()
+            self.LoadMRI.renderers[idx] = {}
+
+        #remove old renderers
+        for image_index,vtk_widget_image in self.LoadMRI.vtk_widgets.items():
+            for view_name, vtk_widget in vtk_widget_image.items():
+                ren_win = vtk_widget.GetRenderWindow()
+                ren_coll = ren_win.GetRenderers()
+
+                renderers_to_remove = [ren_coll.GetItemAsObject(i) for i in range(ren_coll.GetNumberOfItems())]
+
+                for old_renderer in renderers_to_remove:
+                    ren_win.RemoveRenderer(old_renderer)
+
+        #load file again, update cursor
+        self.LoadMRI = LoadMRI(MW)
+
+        MW.ui.comboBox_resamplefiles.addItem(os.path.basename(self.file_name100um)) #add to combobox for resampling
+        data_view = "coronal" #for 3d data
+        buttons_gui3d.popup.close()
+        MW.restart_gui(self.file_name100um, data_view)
+
+
+
+
+    @staticmethod
+    def resampling50um_trajectoryPlanning(file_name,new_spacing_mm=0.05):
+        """
+            Resample the main MRI image to a 100umx100umx100um MRI image.
+            Applies padding at the end to avoid black slices after resampling.
+        """
+        img = sitk.ReadImage(file_name)
+        old_spacing = img.GetSpacing() #x,y,z
+        old_size = img.GetSize()
+        # first resample in z
+        new_spacing = [old_spacing[0],old_spacing[1],new_spacing_mm] #x,y,z
+
+        new_size = [
+            int(round(osz * ospc / nspc))
+            for osz, ospc, nspc in zip(old_size, old_spacing, new_spacing)
+        ]
+
+        new_newspacing = [
+            old_sz * old_sp / new_sz
+            for old_sz, old_sp, new_sz in zip(old_size, old_spacing, new_size)
+        ]
+
+        # Get last slice intensities to avoid blackness in the last slides
+        last_slice_array = sitk.GetArrayFromImage(img)[-1,:,:]
+        edge_value = float(last_slice_array.mean())
+
+
+        last_slice = img[:, :, -1]
+
+        # Repeat the last slice N times to extend volume
+        origin = img.GetOrigin()
+        pad_slices = 20 # number of repeated slices
+        replicated = [last_slice] * pad_slices
+        replicated_stack = sitk.JoinSeries(replicated)
+        replicated_stack.SetSpacing((old_spacing[0], old_spacing[1], old_spacing[2]))
+        replicated_stack.SetOrigin((origin[0], origin[1], origin[2] + old_spacing[2]*old_size[2]))
+        replicated_stack.SetDirection(img.GetDirection())
+
+        image_padded = sitk.Paste(
+            sitk.ConstantPad(img, [0, 0, 0], [0, 0, pad_slices], 0),
+            replicated_stack,
+            replicated_stack.GetSize(),
+            destinationIndex=[0, 0, img.GetSize()[2]]
+        )
+
+        resampled_z = sitk.Resample(
+            image_padded,
+            new_size,
+            sitk.Transform(),
+            sitk.sitkLinear,
+            image_padded.GetOrigin(),
+            new_newspacing,
+            image_padded.GetDirection(),
+            edge_value,
+            image_padded.GetPixelID()
+        )
+        # resample in y
+        old_spacing = resampled_z.GetSpacing()
+        old_size = resampled_z.GetSize()
+        new_spacing = [old_spacing[0],new_spacing_mm,old_spacing[2]] #x,y,z
+
+        new_size = [
+            int(round(osz * ospc / nspc))
+            for osz, ospc, nspc in zip(old_size, old_spacing, new_spacing)
+        ]
+
+        new_newspacing = [
+            old_sz * old_sp / new_sz
+            for old_sz, old_sp, new_sz in zip(old_size, old_spacing, new_size)
+        ]
+
+        # Get last slice intensities to avoid blackness in the last slides
+        last_slice_array = sitk.GetArrayFromImage(resampled_z)[:,-1,:]
+        edge_value = float(last_slice_array.mean())
+
+        img = resampled_z
+        last_slice = img[:, -1:, :]
+
+        ## Repeat the last slice N times to extend volume
+        origin = resampled_z.GetOrigin()
+        pad_slices = 20  # number of repeated slices
+        img_padded = sitk.ConstantPad(
+            img,
+            padLowerBound=[0, 0, 0],
+            padUpperBound=[0, pad_slices, 0],
+            constant=0  # temporary, will be overwritten
+        )
+
+        last_slice = sitk.Extract(
+            img_padded,
+            size=[img.GetSize()[0], 1, img.GetSize()[2]],
+            index=[0, img.GetSize()[1]-1, 0]
+        )
+
+        image_padded = img_padded
+        for i in range(pad_slices):
+            image_padded = sitk.Paste(
+                destinationImage=img_padded,
+                sourceImage=last_slice,
+                sourceSize=last_slice.GetSize(),
+                destinationIndex=[0, img.GetSize()[1]+i, 0]
+            )
+
+        resampled_zy = sitk.Resample(
+            image_padded,
+            new_size,
+            sitk.Transform(),
+            sitk.sitkLinear,
+            image_padded.GetOrigin(),
+            new_newspacing,
+            image_padded.GetDirection(),
+            edge_value,
+            image_padded.GetPixelID()
+        )
+
+        # resample in x
+        old_spacing = resampled_zy.GetSpacing()
+        old_size = resampled_zy.GetSize()
+        new_spacing = [new_spacing_mm, old_spacing[1],old_spacing[2]]
+
+        new_size = [
+            int(round(osz * ospc / nspc))
+            for osz, ospc, nspc in zip(old_size, old_spacing, new_spacing)
+        ]
+
+        new_newspacing = [
+            old_sz * old_sp / new_sz
+            for old_sz, old_sp, new_sz in zip(old_size, old_spacing, new_size)
+        ]
+
+        # Get last slice intensities to avoid blackness in the last slides
+        last_slice_array = sitk.GetArrayFromImage(resampled_zy)[-1,:,:]
+        edge_value = float(last_slice_array.mean())
+
+        img = resampled_zy
+        last_slice = img[-1:, :, :]
+
+        ## Repeat the last slice N times to extend volume
+        origin = resampled_zy.GetOrigin()
+        pad_slices = 20  # number of repeated slices
+        img_padded = sitk.ConstantPad(
+            img,
+            padLowerBound=[0, 0, 0],
+            padUpperBound=[pad_slices, 0, 0],
+            constant=0  # temporary, will be overwritten
+        )
+
+        last_slice = sitk.Extract(
+            img_padded,
+            size=[1, img.GetSize()[1], img.GetSize()[2]],
+            index=[img.GetSize()[0]-1, 0, 0]
+        )
+
+        image_padded_x = img_padded
+        for i in range(pad_slices):
+            image_padded_x = sitk.Paste(
+                destinationImage=image_padded_x,
+                sourceImage=last_slice,
+                sourceSize=last_slice.GetSize(),
+                destinationIndex=[img.GetSize()[0]+i,0, 0]
+            )
+
+        resampled = sitk.Resample(
+            image_padded_x,
+            new_size,
+            sitk.Transform(),
+            sitk.sitkLinear,
+            image_padded_x.GetOrigin(),
+            new_newspacing,
+            image_padded_x.GetDirection(),
+            edge_value,
+            image_padded_x.GetPixelID()
+        )
+
+        filename_end = f"resampled{new_spacing_mm*1000:.10g}um.nii.gz"
+
+        default_name = f"{file_name[:-7]}_{filename_end}"
+        save_path = os.path.join(default_name)
+        sitk.WriteImage(resampled, save_path)
+
+        return save_path
+
